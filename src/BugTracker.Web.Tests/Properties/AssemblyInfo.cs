22afb88e79b3ead9ab4ed60fb7459d37583c9cda
Ivan Grek <kvant@outlook.com>
2019-09-02 18:54:31 +0300

    Add project for unit tests

---
+++ src/BugTracker.Web.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BugTracker.Web.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BugTracker.Web.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("14958366-8b67-4a0d-88e5-4a573721f88c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/BugTracker.Web.Tests/Tests.cs
@@ -0,0 +1,25 @@
+﻿/*
+  Copyright 2017-2019 Ivan Grek
+
+  Distributed under the terms of the GNU General Public License
+*/
+
+namespace BugTracker.Web.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test_should_work()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.That(true, Is.True);
+        }
+    }
+}
