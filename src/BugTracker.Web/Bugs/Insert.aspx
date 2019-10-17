c7ccf2e9cded96b5725e0145dac7ee2c25ffc8e2
Ivan Grek <kvant@outlook.com>
2019-10-17 16:20:21 +0300

    Migrate Bugs/Insert.aspx to MVC

--- src/BugTracker.Web/Bugs/Insert.aspx
+++
@@ -1,9 +0,0 @@
-<%--
-    Copyright 2002-2011 Corey Trager
-    Copyright 2017-2019 Ivan Grek
-
-    Distributed under the terms of the GNU General Public License
---%>
-
-<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Insert.aspx.cs" Inherits="BugTracker.Web.Bugs.Insert" %>
-<%@ Assembly Name="SharpMimeTools" %>
--- src/BugTracker.Web/Controllers/BugController.cs
+++ src/BugTracker.Web/Controllers/BugController.cs
@@ -7,14 +7,15 @@
 
 namespace BugTracker.Web.Controllers
 {
+    using anmar.SharpMimeTools;
     using BugTracker.Web.Core;
     using BugTracker.Web.Models;
     using BugTracker.Web.Models.Bug;
     using System;
     using System.Collections.Generic;
-    using System.Collections.Specialized;
     using System.Data;
     using System.Data.SqlClient;
+    using System.IO;
     using System.Text;
     using System.Web.Mvc;
     using System.Web.UI;
@@ -25,13 +26,16 @@ namespace BugTracker.Web.Controllers
     {
         private readonly IApplicationSettings applicationSettings;
         private readonly ISecurity security;
+        private readonly IAuthenticate authenticate;
 
         public BugController(
             IApplicationSettings applicationSettings,
-            ISecurity security)
+            ISecurity security,
+            IAuthenticate authenticate)
         {
             this.applicationSettings = applicationSettings;
             this.security = security;
+            this.authenticate = authenticate;
         }
 
         [HttpGet]
@@ -134,6 +138,327 @@ namespace BugTracker.Web.Controllers
             }
 
             return RedirectToAction(nameof(Index));
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public ActionResult Insert()
+        {
+            var username = Request["username"];
+            var password = Request["password"];
+            var projectidString = Request["projectid"];
+            var comment = Request["comment"];
+            var fromAddr = Request["from"];
+            var cc = "";
+            var message = Request["message"];
+            var attachmentAsBase64 = Request["attachment"];
+            var attachmentContentType = Request["attachment_content_type"];
+            var attachmentFilename = Request["attachment_filename"];
+            var attachmentDesc = Request["attachment_desc"];
+            var bugidString = Request["bugid"];
+            var shortDesc = Request["short_desc"];
+
+            // this could also be the email subject
+            if (shortDesc == null)
+            {
+                shortDesc = string.Empty;
+            }
+            else if (shortDesc.Length > 200)
+            {
+                shortDesc = shortDesc.Substring(0, 200);
+            }
+
+            SharpMimeMessage mimeMessage = null;
+
+            if (message != null && message.Length > 0)
+            {
+                mimeMessage = MyMime.GetSharpMimeMessage(message);
+
+                comment = MyMime.GetComment(mimeMessage);
+
+                var headers = MyMime.GetHeadersForComment(mimeMessage);
+
+                if (headers != string.Empty)
+                {
+                    comment = headers + "\n" + comment;
+                }
+
+                fromAddr = MyMime.GetFromAddr(mimeMessage);
+            }
+            else
+            {
+                if (comment == null) comment = "";
+            }
+
+            if (string.IsNullOrEmpty(username))
+            {
+                Response.AddHeader("BTNET", "ERROR: username required");
+
+                return Content("ERROR: username required");
+            }
+
+            if (string.IsNullOrEmpty(password))
+            {
+                Response.AddHeader("BTNET", "ERROR: password required");
+
+                return Content("ERROR: password required");
+            }
+
+            // authenticate user
+
+            var authenticated = this.authenticate.CheckPassword(username, password);
+
+            if (!authenticated)
+            {
+                Response.AddHeader("BTNET", "ERROR: invalid username or password");
+
+                return Content("ERROR: invalid username or password");
+            }
+
+            var security = MyMime.GetSynthesizedSecurity(mimeMessage, fromAddr, username);
+
+            var projectid = 0;
+            if (Util.IsInt(projectidString)) projectid = Convert.ToInt32(projectidString);
+
+            var bugid = 0;
+
+            if (Util.IsInt(bugidString))
+            {
+                bugid = Convert.ToInt32(bugidString);
+            }
+
+            // Even though btnet_service.exe has already parsed out the bugid,
+            // we can do a better job here with SharpMimeTools.dll
+            var subject = string.Empty;
+
+            if (mimeMessage != null)
+            {
+                subject = MyMime.GetSubject(mimeMessage);
+
+                if (subject != "[No Subject]") bugid = MyMime.GetBugidFromSubject(ref subject);
+
+                cc = MyMime.GetCc(mimeMessage);
+            }
+
+            var sql = string.Empty;
+
+            if (bugid != 0)
+            {
+                // Check if the bug is still in the database
+                // No comment can be added to merged or deleted bugids
+                // In this case a new bug is created, this to prevent possible loss of information
+
+                sql = @"select count(bg_id)
+                    from bugs
+                    where bg_id = $id";
+
+                sql = sql.Replace("$id", Convert.ToString(bugid));
+
+                if (Convert.ToInt32(DbUtil.ExecuteScalar(sql)) == 0)
+                {
+                    bugid = 0;
+                }
+            }
+
+            // Either insert a new bug or append a commment to existing bug
+            // based on presence, absence of bugid
+            if (bugid == 0)
+            {
+                // insert a new bug
+
+                if (mimeMessage != null)
+                {
+                    // in case somebody is replying to a bug that has been deleted or merged
+                    subject = subject.Replace(this.applicationSettings.TrackingIdString, "PREVIOUS:");
+
+                    shortDesc = subject;
+                    if (shortDesc.Length > 200)
+                    {
+                        shortDesc = shortDesc.Substring(0, 200);
+                    }
+                }
+
+                var orgid = 0;
+                var categoryid = 0;
+                var priorityid = 0;
+                var assignedid = 0;
+                var statusid = 0;
+                var udfid = 0;
+
+                // You can control some more things from the query string
+                if (!string.IsNullOrEmpty(Request["$ORGANIZATION$"]))
+                {
+                    orgid = Convert.ToInt32(Request["$ORGANIZATION$"]);
+                }
+
+                if (!string.IsNullOrEmpty(Request["$CATEGORY$"]))
+                {
+                    categoryid = Convert.ToInt32(Request["$CATEGORY$"]);
+                }
+
+                if (!string.IsNullOrEmpty(Request["$PROJECT$"]))
+                {
+                    projectid = Convert.ToInt32(Request["$PROJECT$"]);
+                }
+
+                if (!string.IsNullOrEmpty(Request["$PRIORITY$"]))
+                {
+                    priorityid = Convert.ToInt32(Request["$PRIORITY$"]);
+                }
+
+                if (!string.IsNullOrEmpty(Request["$ASSIGNEDTO$"]))
+                {
+                    assignedid = Convert.ToInt32(Request["$ASSIGNEDTO$"]);
+                }
+
+                if (!string.IsNullOrEmpty(Request["$STATUS$"]))
+                {
+                    statusid = Convert.ToInt32(Request["$STATUS$"]);
+                }
+
+                if (!string.IsNullOrEmpty(Request["$UDF$"]))
+                {
+                    udfid = Convert.ToInt32(Request["$UDF$"]);
+                }
+
+                var defaults = Bug.GetBugDefaults();
+
+                // If you didn't set these from the query string, we'll give them default values
+                if (projectid == 0) projectid = (int)defaults["pj"];
+                if (orgid == 0) orgid = security.User.Org;
+                if (categoryid == 0) categoryid = (int)defaults["ct"];
+                if (priorityid == 0) priorityid = (int)defaults["pr"];
+                if (statusid == 0) statusid = (int)defaults["st"];
+                if (udfid == 0) udfid = (int)defaults["udf"];
+
+                // but forced project always wins
+                if (security.User.ForcedProject != 0) projectid = security.User.ForcedProject;
+
+                var newIds = Bug.InsertBug(
+                    shortDesc,
+                    security,
+                    string.Empty, // tags
+                    projectid,
+                    orgid,
+                    categoryid,
+                    priorityid,
+                    statusid,
+                    assignedid,
+                    udfid,
+                    string.Empty, string.Empty, string.Empty, // project specific dropdown values
+                    comment,
+                    comment,
+                    fromAddr,
+                    cc,
+                    "text/plain",
+                    false, // internal only
+                    null, // custom columns
+                    false); // suppress notifications for now - wait till after the attachments
+
+                if (mimeMessage != null)
+                {
+                    MyMime.AddAttachments(mimeMessage, newIds.Bugid, newIds.Postid, security);
+
+                    MyPop3.AutoReply(newIds.Bugid, fromAddr, shortDesc, projectid);
+                }
+                else if (attachmentAsBase64 != null && attachmentAsBase64.Length > 0)
+                {
+                    if (attachmentDesc == null) attachmentDesc = string.Empty;
+                    if (attachmentContentType == null) attachmentContentType = string.Empty;
+                    if (attachmentFilename == null) attachmentFilename = string.Empty;
+
+                    var byteArray = Convert.FromBase64String(attachmentAsBase64);
+                    var stream = new MemoryStream(byteArray);
+
+                    Bug.InsertPostAttachment(
+                        security,
+                        newIds.Bugid,
+                        stream,
+                        byteArray.Length,
+                        attachmentFilename,
+                        attachmentDesc,
+                        attachmentContentType,
+                        -1, // parent
+                        false, // internal_only
+                        false); // don't send notification yet
+                }
+
+                // your customizations
+                Bug.ApplyPostInsertRules(newIds.Bugid);
+
+                Bug.SendNotifications(Bug.Insert, newIds.Bugid, security);
+                WhatsNew.AddNews(newIds.Bugid, shortDesc, "added", security);
+
+                Response.AddHeader("BTNET", "OK:" + Convert.ToString(newIds.Bugid));
+
+                return Content("OK:" + Convert.ToString(newIds.Bugid));
+            }
+            else // update existing bug
+            {
+                var statusResultingFromIncomingEmail = this.applicationSettings.StatusResultingFromIncomingEmail;
+
+                sql = string.Empty;
+
+                if (statusResultingFromIncomingEmail != 0)
+                {
+                    sql = @"update bugs
+                        set bg_status = $st
+                        where bg_id = $bg";
+
+                    sql = sql.Replace("$st", statusResultingFromIncomingEmail.ToString());
+                }
+
+                sql += "select bg_short_desc from bugs where bg_id = $bg";
+
+                sql = sql.Replace("$bg", Convert.ToString(bugid));
+                var dr2 = DbUtil.GetDataRow(sql);
+
+                // Add a comment to existing bug.
+                var postid = Bug.InsertComment(
+                    bugid,
+                    security.User.Usid, // (int) dr["us_id"],
+                    comment,
+                    comment,
+                    fromAddr,
+                    cc,
+                    "text/plain",
+                    false); // internal only
+
+                if (mimeMessage != null)
+                {
+                    MyMime.AddAttachments(mimeMessage, bugid, postid, security);
+                }
+                else if (attachmentAsBase64 != null && attachmentAsBase64.Length > 0)
+                {
+                    if (attachmentDesc == null) attachmentDesc = string.Empty;
+                    if (attachmentContentType == null) attachmentContentType = string.Empty;
+                    if (attachmentFilename == null) attachmentFilename = string.Empty;
+
+                    var byteArray = Convert.FromBase64String(attachmentAsBase64);
+
+                    using (var stream = new MemoryStream(byteArray))
+                    {
+                        Bug.InsertPostAttachment(
+                            security,
+                            bugid,
+                            stream,
+                            byteArray.Length,
+                            attachmentFilename,
+                            attachmentDesc,
+                            attachmentContentType,
+                            -1, // parent
+                            false, // internal_only
+                            false); // don't send notification yet
+                    }
+                }
+
+                Bug.SendNotifications(Bug.Update, bugid, security);
+                WhatsNew.AddNews(bugid, (string)dr2["bg_short_desc"], "updated", security);
+
+                Response.AddHeader("BTNET", "OK:" + Convert.ToString(bugid));
+
+                return Content("OK:" + Convert.ToString(bugid));
+            }
         }
 
         [HttpGet]
