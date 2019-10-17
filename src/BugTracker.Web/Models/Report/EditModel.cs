20e4a9831440fc9024b685f10a64b853ff860d37
Ivan Grek <kvant@outlook.com>
2019-10-17 13:19:22 +0300

    Change layout for Report section

--- src/BugTracker.Web/Models/Report/EditModel.cs
+++ src/BugTracker.Web/Models/Report/EditModel.cs
@@ -12,12 +12,15 @@ namespace BugTracker.Web.Models.Report
     {
         public int Id { get; set; }
 
+        [Display(Name = "Name")]
         [Required(ErrorMessage = "Description is required.")]
         public string Name { get; set; }
 
+        [Display(Name = "Chart Type")]
         [Required(ErrorMessage = "Chart Type is required.")]
         public string ChartType { get; set; }
 
+        [Display(Name = "SQL")]
         [Required(ErrorMessage = "The SQL statement is required.")]
         public string SqlText { get; set; }
     }
