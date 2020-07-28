e211e687d14b3140fbe7dc66b745c0116c39006d
Ivan Grek <kvant@outlook.com>
2020-07-29 01:05:08 +0300

    Migrate some project files to vs2017 format

--- test/BugTracker.Web.Tests/Properties/AssemblyInfo.cs
+++ test/BugTracker.Web.Tests/Properties/AssemblyInfo.cs
@@ -1,19 +1,6 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("BugTracker.Web.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BugTracker.Web.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("14958366-8b67-4a0d-88e5-4a573721f88c")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
