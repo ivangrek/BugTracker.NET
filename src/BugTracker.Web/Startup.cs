400eca0622632c5a589a62a40089f6cfc720ea89
Ivan Grek <kvant@outlook.com>
2020-08-22 02:59:56 +0300

    fix

--- src/BugTracker.Web/Startup.cs
+++ src/BugTracker.Web/Startup.cs
@@ -28,7 +28,7 @@ namespace BugTracker.Web
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello, world from BugTracker.NET Core!");
                 });
             });
         }
