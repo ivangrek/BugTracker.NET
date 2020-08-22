ceebb2cb005b3f94bc07a738b463b8df5e342fd6
Ivan Grek <kvant@outlook.com>
2020-08-22 17:02:30 +0300

    Add mvc

--- src/BugTracker.Web/Startup.cs
+++ src/BugTracker.Web/Startup.cs
@@ -12,6 +12,7 @@ namespace BugTracker.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,10 +27,9 @@ namespace BugTracker.Web
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello, world from BugTracker.NET Core!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
